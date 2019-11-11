d79718d7f29297baa781bee27f1291052f054558
Isak Hauge <isakhauge@gmail.com>
2019-11-10 22:48:05 -0800

    Create db_test route for testing.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
+const MySQL = require('../res/class/mysql/MySQL');
 var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", {
-    title: "Final Project"
+    title: "Final Project",
+  });
+});
+
+// DB Test page.
+router.get('/db_test', (req, res, nex) => {
+  const db = new MySQL();
+  db.query('SHOW PROCESSLIST', (result) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.json(result);
   });
 });
 
