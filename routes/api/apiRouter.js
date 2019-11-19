4641d1bc949c2a37e4e5486819e2db6b6e2317c8
Isak Hauge <isakhauge@gmail.com>
2019-11-18 21:33:56 -0800

    Update API routers.

--- routes/api/apiRouter.js
+++ routes/api/apiRouter.js
@@ -1,5 +1,3 @@
-const MySQL = require('../../res/class/mysql/MySQL');
-const User = require('../../res/class/orm/User');
 const express = require("express");
 const apiRouter = express.Router();
 const userApi = require('./user/userRouter');
--- routes/api/user/userRouter.js
+++ routes/api/user/userRouter.js
@@ -1,141 +1,111 @@
-const MySQL = require('../../../res/class/mysql/MySQL');
-const User = require('../../../res/class/orm/User');
 const express = require("express");
-const user = express.Router();
-const StatusObject = require('../../../res/class/api/StatusObject');
+const userApi = express.Router();
+const user = require('../../../res/class/api/orm/User');
+const statusObject = require('../../../res/class/api/StatusObject');
+const responseBody = require('../../../res/class/api/orm/ResponseBody');
+const crudOperation = require('../../../res/class/api/orm/CrudOperation');
 
 // * Get all users.
-user.get('/', (req, res, next) => {
-	User.fetchAll((result) => {
-		const argValidate = {
-			successCondition: result !== '[]',
-			operation: 'get',
-			entity: 'user',
-			successText: `All users were successfully fetched.`,
-			failText: `There are no users in the database.`,
-		};
-		StatusObject.validate(argValidate, (status) => {
-			const argProcess = {
-				mysqlResult: result,
-				statusObject: status
-			};
-			StatusObject.produce(argProcess, (out) => {
-				console.log(out);
-				res.json(out);
-			});
-		});
+userApi.get('/', (req, res, next) => {
+	user.fetchAll((result) => {
+		res.json(
+			new responseBody(
+				'user',
+				crudOperation.READ,
+				Array.isArray(result) && result.length > 0,
+				`${result.length} users were successfully fetched from the database.`,
+				'No users found.',
+				result,
+			)
+		);
 	})
 });
 
 // * Get specific user.
-user.get('/:email', (req, res, next) => {
+userApi.get('/:email', (req, res, next) => {
 	const {email} = req.params;
-	User.fetch(email, (result) => {
-		const argValidate = {
-			successCondition: result !== '[]',
-			operation: 'get',
-			entity: 'user',
-			successText: `User (${email}) was successfully fetched`,
-			failText: `User (${email}) was not found.`,
-		};
-		StatusObject.validate(argValidate, (status) => {
-			const argProcess = {
-				mysqlResult: result,
-				statusObject: status
-			};
-			StatusObject.produce(argProcess, (out) => {
-				console.log(out);
-				res.json(out);
-			});
-		});
+	user.fetch(email, (result) => {
+		if (!result) res.json([]);
+		res.json(
+			new responseBody(
+				'user',
+				crudOperation.READ,
+				Array.isArray(result) && result.length > 0,
+				`User (${email}) was successfully fetched`,
+				`User (${email}) was not found.`,
+				result
+			)
+		);
 	});
 });
 
 // * Create user.
-user.post('/', (req, res, next) => {
+userApi.post('/', (req, res, next) => {
 	const {name,surname,email,password} = req.body;
-	const argUser = {
+	const requestBody = {
 		name: name,
 		surname: surname,
 		email: email,
 		password: password
 	};
-	User.create(argUser, (result) => {
-		const argValidate = {
-			successCondition: result[1][0]["@out"] === 'User created!',
-			operation: 'create',
-			entity: 'user',
-			successText: `User (${email}) was successfully created`,
-			failText: `Email (${email}) has already been taken.`,
-		};
-		StatusObject.validate(argValidate, (status) => {
-			const argProcess = {
-				mysqlResult: result,
-				statusObject: status
-			};
-			StatusObject.produce(argProcess, (out) => {
-				console.log(out);
-				res.json(out);
-			});
-		});
+	user.create(requestBody, (result) => {
+		if (!result) res.json([]);
+		res.json(
+			new responseBody(
+				'user',
+				crudOperation.CREATE,
+				result[1][0]["@out"] === 'User created!',
+				`User (${email}) was successfully created`,
+				`Email (${email}) has already been taken.`,
+				result[1][0]["@out"]
+			)
+		);
 	});
 });
 
 // * Update user.
-user.put('/:email/:password', (req, res, next) => {
+// TODO: Fix error handling.
+userApi.put('/:email/:password', (req, res, next) => {
 	const {email, password} = req.params;
-	const body = req.body;
-	User.fetch(email, (user) => {
-		if (user.length > 0) {
-			const loginMatch = email === user[0].email && password === user[0].password;
-			if (loginMatch) {
-				User.edit(body, (result) => {
-					const argValidate = {
-						successCondition: result[1][0]["@out"] === 'User updated!',
-						operation: 'update',
-						entity: 'user',
-						successText: `User (${email}) was successfully updated.`,
-						failText: `User (${email}) was not updated. Wrong email or password.`,
-					};
-					StatusObject.validate(argValidate, (status) => {
-						const argProcess = {
-							mysqlResult: result,
-							statusObject: status
-						};
-						StatusObject.produce(argProcess, (out) => {
-							console.log(out);
-							res.json(out);
-						});
-					});
-				});
-			} else {
-				const argValidate = {
-					successCondition: loginMatch,
-					operation: 'update',
-					entity: 'user',
-					successText: `User (${email}) was successfully updated.`,
-					failText: `User (${email}) was not updated. Wrong email or password.`,
-				};
-				StatusObject.validate(argValidate, (status) => {
-					const argProcess = {
-						mysqlResult: [],
-						statusObject: status
-					};
-					StatusObject.produce(argProcess, (out) => {
-						console.log(out);
-						res.json(out);
-					});
-				});
-			}
+	user.fetch(email, (user) => {
+		console.log(user[0]);
+		if (!user || user.length < 1) res.json([]);
+		const loginMatch = email === user[0].email && password === user[0].password;
+		if (loginMatch) {
+			user.edit(req.body, (result) => {
+				if (!result) res.json([]);
+				res.json(
+					new responseBody(
+						'user',
+						crudOperation.UPDATE,
+						result[1][0]["@out"] === 'User updated!',
+						`User (${email}) was successfully updated.`,
+						`User (${email}) was not updated. Wrong email or password.`,
+						result[1][0]["@out"]
+					)
+				);
+			});
+		} else {
+			res.json(
+				new responseBody(
+					'user',
+					crudOperation.UPDATE,
+					loginMatch,
+					'',
+					'Login incorrect.',
+					[],
+				)
+			);
 		}
 	});
 });
 
 // * Delete user.
-user.delete('/:email/:password', (req, res, next) => {
+// TODO: Fix error handling.
+userApi.delete('/:email/:password', (req, res, next) => {
 	const {email, password} = req.params;
 	const credentials = {email: email, password: password};
-	User.delete(credentials, (result) => {
+	user.delete(credentials, (result) => {
 		const argValidate = {
 			successCondition: result[1][0]["@out"] === 'User deleted!',
 			operation: 'delete',
@@ -143,12 +113,12 @@ user.delete('/:email/:password', (req, res, next) => {
 			successText: `User (${email}) was successfully deleted`,
 			failText: `User (${email}) was not deleted. Wrong email or password.`,
 		};
-		StatusObject.validate(argValidate, (status) => {
+		statusObject.validate(argValidate, (status) => {
 			const argProcess = {
 				mysqlResult: result,
 				statusObject: status
 			};
-			StatusObject.produce(argProcess, (out) => {
+			statusObject.produce(argProcess, (out) => {
 				console.log(out);
 				res.json(out);
 			});
@@ -156,4 +126,4 @@ user.delete('/:email/:password', (req, res, next) => {
 	});
 });
 
-module.exports = user;
+module.exports = userApi;
