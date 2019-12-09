8f115f3b7a6599fc4298f63eaec16be891b427b5
ThoMot <thora.marie@outlook.com>
2019-12-09 00:37:21 -0800

    removed collective in route

--- routes/chore.js
+++ routes/chore.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 router.get("/", function(req, res) {
         res.render("page/chore/home", {
-            collective: false,
             title: "Chores"
         });
 });
