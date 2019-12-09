7f3b5aa8adf9e7a9995cc4198c525a5306a713ce
Thora Mothes <thora.marie@outlook.com>
2019-12-09 00:39:10 -0800

    chore add plus small adjustment to edit chore (#47)
    * chore add plus small adjustment to edit chore
    
    * removed collective in route

--- routes/chore.js
+++ routes/chore.js
@@ -3,8 +3,7 @@ const router = express.Router();
 
 router.get("/", function(req, res) {
         res.render("page/chore/home", {
-            collective: true,
-            title: "Collective"
+            title: "Chores"
         });
 });
 
