3416ec70b676d096c4b279051fda55f88dcf9562
Thora Mothes <thora.marie@outlook.com>
2019-12-09 22:35:21 -0800

    added signup function (#64)

---
+++ public/js/signup.js
@@ -0,0 +1,34 @@
+$(document).ready(() => {
+
+    //sign up button
+    $("#signUpBtn").on("click", function () {
+        createUser();
+    });
+
+});
+
+
+function createUser(){
+    $.ajax({
+        url: "/api/user",
+        method: "POST",
+        dataType:  "json",
+        contentType: "application/json",
+        data: JSON.stringify({
+            name: $("#firstname").val(),
+            surname: $("#lastname").val(),
+            email: $("#email").val(),
+            password: $("#password").val()
+        }),
+        success: function (result) {
+            if(result.meta.success){
+                $("#modalRegisterForm").modal("hide");
+                $("#modalSignIn").modal("show");
+            } else {
+                $("#feedbkSignup").html(result.result)
+            }
+
+        }
+
+    });
+}
