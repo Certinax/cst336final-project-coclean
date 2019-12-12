bd71d3d7a0bb3de09917e9e0a511555aeee17ea9
Certinax <s319217@oslomet.no>
2019-12-11 20:20:32 -0800

    Added redirect and username control from express session

--- routes/collective.js
+++ routes/collective.js
@@ -5,7 +5,8 @@ router.get("/", function(req, res) {
   if (req.session.userId) {
     res.render("page/collective/select", {
       collective: true,
-      title: "Collective"
+      title: "Collective",
+      username: req.session.username
     });
   } else {
     res.redirect("/");
@@ -13,17 +14,27 @@ router.get("/", function(req, res) {
 });
 
 router.get("/new", function(req, res) {
-  res.render("page/collective/new", {
-    collective: true,
-    title: "Create Collective"
-  });
+  if (req.session.userId) {
+    res.render("page/collective/new", {
+      collective: true,
+      title: "Create Collective",
+      username: req.session.username
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 
 router.get("/edit", function(req, res) {
-  res.render("page/collective/edit", {
-    collective: true,
-    title: "Edit Collective"
-  });
+  if (req.session.userId) {
+    res.render("page/collective/edit", {
+      collective: true,
+      title: "Edit Collective",
+      username: req.session.username
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
--- routes/profile.js
+++ routes/profile.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function(req, res) {
-  res.render("profile", {
-    profile: true,
-    title: "User Profile",
-    username: "Christina"
-  });
+  if (req.session.userId) {
+    res.render("profile", {
+      profile: true,
+      title: "User Profile",
+      username: req.session.username
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
