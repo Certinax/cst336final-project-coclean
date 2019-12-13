6e321e1263870afe81033b51823fb140f3ebda11
Certinax <31922229+Certinax@users.noreply.github.com>
2019-12-13 22:52:53 +0100

    Feature/i104 statistic v2 (#109)
    * Updated collective creation to make use of insertid
    
    * Users can properly invite and join collectives
    
    * Fixed edit routes
    
    * Statstic page now properly shows stats from database aggregations
    
    * Updated message on report card on first load
    
    * Updated navbar to include statistic page

--- public/js/page/statistics/main.js
+++ public/js/page/statistics/main.js
@@ -1,9 +1,15 @@
 $(document).ready(() => {
-  console.log("Ready from /statistic");
   $("#getStats").on("click", function() {
-    console.log("asd");
+    if ($("#collectiveList").val() !== "") {
+      getInfo();
+    } else {
+      $("#reportData")
+        .empty()
+        .append(
+          "<p class='card-text'><span class='overdueRed'>Select a collective!</span></p>"
+        );
+    }
   });
-
   populateCollectives();
 });
 
@@ -29,16 +35,90 @@ function populateCollectives() {
     dataType: "json",
     contentType: "application/json",
     success: function(result) {
-      console.log(result);
-      $("#collective").empty();
-      result.result.map(collective => {
-        $("#collective").append(
-          `<option value=${collective.name}>${collective.name}</option>`
-        );
-      });
+      if (result.meta.success) {
+        $("#collective").empty();
+        result.result.map(collective => {
+          $("#collectiveList").append(
+            `<option value=${collective.ID}>${collective.name}</option>`
+          );
+        });
+      }
     },
     error: function(error) {
       console.log(error);
     }
   });
+}
+
+function getInfo() {
+  const endpoint = $("#stats").val();
+  $.ajax({
+    url: `statistics/${endpoint}`,
+    method: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify({
+      id: $("#collectiveList").val()
+    }),
+    success: function(result) {
+      if (endpoint === "members") {
+        showMembers(result);
+      } else if (endpoint === "overdues") {
+        showOverdues(result);
+      } else if (endpoint === "completed") {
+        showCompleted(result);
+      }
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+}
+
+function showMembers(members) {
+  const title = "Members in " + $("#collectiveList option:selected").text();
+  const numOfMembers = members[0].NumOfMembers;
+  $("#reportData")
+    .empty()
+    .append(`<h5 class="card-title">${title}</h5>`)
+    .append(
+      `<p class="card-text">Currently there are ${numOfMembers} people in this collective. </p>`
+    )
+    .append("<hr/>");
+}
+
+function showOverdues(overdues) {
+  const title = "Overdues in " + $("#collectiveList option:selected").text();
+  $("#reportData")
+    .empty()
+    .append(`<h5 class="card-title">${title}</h5>`);
+
+  $("#reportData").append('<ul class="list-group"></ul>');
+  overdues.map(person => {
+    $("#reportData ul").append(
+      `<li class="list-group-item d-flex justify-content-between align-items-center">${person.name}<span class="badge badge-primary badge-pill">${person.NumOfOverdues}</span></li>`
+    );
+  });
+  $("#reportData").append("<hr class='pb-3'/>");
+}
+
+function showCompleted(completed) {
+  const title = "Overdues in " + $("#collectiveList option:selected").text();
+  const completion = completed[0].PercentageCompleted;
+  $("#reportData")
+    .empty()
+    .append(`<h5 class="card-title">${title}</h5>`);
+
+  if (!completion) {
+    $("#reportData")
+      .append(`<p class="card-text">This collective have no chores.</p>`)
+      .append("<hr/>");
+  } else {
+    const completionRate = Number.parseInt(completion).toFixed(1);
+    $("#reportData")
+      .append(
+        `<p class="card-text">The completion rate for chores in this collective is at ${completionRate}%.</p>`
+      )
+      .append("<hr/>");
+  }
 }
